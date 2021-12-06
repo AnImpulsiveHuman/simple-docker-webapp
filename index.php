2feaacf13e9d47fd6524957c80ef2da2a97a85ae
vishalpranav03 <64655668+vishalpranav03@users.noreply.github.com>
2021-12-06 16:54:27 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
-         $dbhost = 'localhost';
-         $dbuser = 'root';
-	 $dbpass = 'ko';
-	 $dbname = 'test123';
-	 $port   = '3306';
-         $mysqli = new mysqli($dbhost, $dbuser, $dbpass, $dbname, $port);
+         //$dbhost = 'localhost';
+         //$dbuser = 'root';
+	 //$dbpass = 'ko';
+	 //$dbname = 'test123';
+	 //$port   = '3306';
+         $mysqli = new mysqli("localhost", "root", "ko", "test123", "3306");
          
          if($mysqli->connect_errno ) {
             printf("Connect failed: %s<br />", $mysqli->connect_error);
